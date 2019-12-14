8f468d2326e671d8b63d9d101db35ed3ce6b2a37
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-12-13 19:33:39 -0600

    Create FGFR1.DMG.meta.R

---
+++ fgfr1/FGFR1.DMG.meta.R
@@ -0,0 +1,157 @@
+
+id2phen4<-function(filename){
+  library("stringr")
+  as.array(str_extract(filename,"TCGA-[0-9|a-z|A-Z]*-[0-9|a-z|A-Z]*-[0-9]*"))
+}
+
+id2phen3<-function(filename){
+  library("stringr")
+  as.array(str_extract(filename,"TCGA-[0-9|a-z|A-Z]*-[0-9|a-z|A-Z]*"))
+}
+
+id2bin<-function(filename){
+  library("stringr")
+  filename<-as.array(str_extract(filename,"TCGA-[0-9|a-z|A-Z]*-[0-9|a-z|A-Z]*-[0-9]*"))
+  as.numeric(lapply(strsplit(filename,"-"),function(x) x[4]))
+}
+
+RawNARemove<-function(data,missratio=0.3){
+  threshold<-(missratio)*ncol(data)
+  NaRaw<-which(apply(data,1,function(x) sum(is.na(x))>=threshold))
+  zero<-which(apply(data,1,function(x) all(x==0))==T)
+  NaRAW<-c(NaRaw,zero)
+  if(length(NaRAW)>0){
+    output<-data[-NaRAW,]
+  }else{
+    output<-data;
+  }
+  output
+}
+
+RawZeroRemove<-function(data,missratio=0.5){
+  threshold<-(missratio)*ncol(data)
+  NaRaw<-which(apply(data,1,function(x) sum(is.na(x))>=threshold))
+  zero<-which(apply(data,1,function(x) sum(x==0)>=threshold))
+  NaRAW<-c(NaRaw,zero)
+  if(length(NaRAW)>0){
+    output<-data[-NaRAW,]
+  }else{
+    output<-data;
+  }
+  output
+}
+
+
+Symbol2ENSG<-function(Symbol){
+  db<-read.table("https://raw.githubusercontent.com/Shicheng-Guo/AnnotationDatabase/master/ENSG.ENST.ENSP.Symbol.hg19.bed",sep="\t")
+  ENSG<-as.character(db[match(Symbol,db$V4),8])
+  ENSG<-na.omit(data.frame(Symbol,ENSG))
+  return(ENSG)
+}
+ENSG2Symbol<-function(ENSG){
+  db<-read.table("https://raw.githubusercontent.com/Shicheng-Guo/AnnotationDatabase/master/ENSG.ENST.ENSP.Symbol.hg19.bed",sep="\t")
+  ENSG<-unlist(lapply(strsplit(ENSG,split="[.]"),function(x) x[1]))
+  Symbol<-db[match(as.character(ENSG),db$V8),4]
+  return(Symbol)
+}
+
+library("metafor")
+
+setwd("~/hpc/methylation/Pancancer/RNA-seq")
+
+load("rnaseqdata.pancancer.RData")
+phen1=read.table("~/hpc/methylation/TCGA-clinical-11093.tsv",header = T,sep="\t")
+phen2=read.table("~/hpc/methylation/Pancancer/RNA-seq/File_metadata2.txt",header = T,sep="\t")
+phen<-data.frame(phen2,phen1[match(phen2$cases.0.case_id,phen1$case_id),])
+phen$file_name=gsub(".gz","",phen$file_name)
+head(phen1)
+head(phen2)
+head(phen)
+
+colnames(rnaseqdata)<-unlist(lapply(strsplit(colnames(rnaseqdata),"/"),function(x) x[2]))
+phen<-phen[match(colnames(rnaseqdata),phen$file_name),]
+
+phen1<-id2phen4(phen$cases.0.samples.0.submitter_id)
+phen2<-id2phen3(phen$cases.0.samples.0.submitter_id)
+phen3<-id2bin(phen$cases.0.samples.0.submitter_id)
+
+phen$bin=phen3
+
+include<-which(c(phen$bin==1 |phen$bin==11))
+phen<-phen[include,]
+input<-rnaseqdata[,include]
+phen$id=id2phen4(phen$cases.0.samples.0.submitter_id)
+dim(phen)
+dim(input)
+head(phen)
+colnames(input)<-phen$id
+Seq<-paste(phen$project_id,phen$bin,sep="-")
+pid<-phen$project_id
+input[1:5,1:5]
+phen[1:5,1:5]
+dim(input)
+dim(phen)
+
+data<-input
+
+Seq<-paste(phen$project_id,phen$bin,sep="-")
+mean<-tapply(as.numeric(data[i,]),Seq,function(x) mean(x,na.rm=T))
+sd<-tapply(as.numeric(data[i,]),Seq,function(x) sd(x,na.rm=T))
+num<-tapply(as.numeric(data[i,]),Seq,function(x) length(x))
+
+exclude <-names(which(table(unlist(lapply(strsplit(names(mean),"-"),function(x) x[2])))<2))
+exclude <-grep(paste(exclude,collapse="|"),phen$project_id)
+
+phen<-phen[-exclude,]
+input<-input[,-exclude]
+phen$id=id2phen4(phen$cases.0.samples.0.submitter_id)
+colnames(input)<-phen$id
+dim(phen)
+dim(input)
+input[1:5,1:5]
+phen[1:5,1:5]
+
+input<-log(input+1,2)
+input<-RawNARemove(input)
+input<-RawZeroRemove(input)
+Seq<-paste(phen$project_id,phen$bin,sep="-")
+
+ENSG<-Symbol2ENSG(c("FGFR1","FGFR2","FGFR3","FGFR4"))
+xgene<-c(as.character(ENSG[,2]))
+j<-unlist(lapply(xgene,function(x) grep(x,rownames(input))))
+print(j)
+
+rlt<-c()
+coll<-c()
+
+for(i in j){
+  print(i)
+  mean<-tapply(as.numeric(input[i,]),Seq,function(x) mean(x,na.rm=T))
+  sd<-tapply(as.numeric(input[i,]),Seq,function(x) sd(x,na.rm=T))
+  num<-tapply(as.numeric(input[i,]),Seq,function(x) length(x))
+  m1i=mean[seq(1,length(mean),by=2)]
+  m2i=mean[seq(2,length(mean),by=2)]
+  sd1i=sd[seq(1,length(mean),by=2)]
+  sd2i=sd[seq(2,length(mean),by=2)]
+  n1i=num[seq(1,length(mean),by=2)]
+  n2i=num[seq(2,length(mean),by=2)]
+  Source<-unlist(lapply(strsplit(names(m1i),"_"),function(x) x[1]))
+  output<-data.frame(cbind(n1i,m1i,sd1i,n2i,m2i,sd2i))
+  output$source=Source
+  output<-na.omit(output)
+  es<-escalc(m1i=m1i, sd1i=sd1i, n1i=n1i, m2i=m2i, sd2i=sd2i, n2i=n2i,measure="MD",data=output)
+  md <- rma(es,slab=source,method = "REML", measure = "SMD",data=output)
+  symbol<-as.character(ENSG2Symbol(rownames(input)[i]))
+  pdf(paste("../../",symbol,"-",rownames(input)[i],".pdf",sep=""))
+  plot(md)
+  dev.off()
+  rlt<-rbind(rlt,c(i,md$beta,md$pval,md$ci.lb,md$ci.ub,md$I2,md$tau2))
+  coll<-c(coll,i)
+}
+
+rownames(rlt)<-rownames(input)[coll]
+colnames(rlt)<-c("idx","beta","pval","cilb","ciub","i2","tau2")
+rlt<-data.frame(rlt)
+head(rlt[order(rlt$pval),])
+
+write.table(rlt,file="FGFR.meta.table.pvalue.txt",sep="\t",quote=F,col.names = NA,row.names = T)
