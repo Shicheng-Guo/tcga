ebb0e58dadd68c5e116e34e0cb43477f9bc82c51
Shicheng Guo <Shicheng.Guo@hotmail.com>
2020-01-30 01:42:12 -0600

    Create NCOA4.R

---
+++ extdata/NCOA4.R
@@ -0,0 +1,15 @@
+## NCOA4
+setwd("/home/guosa/hpc/project/esopheagul")
+setwd("/home/guosa/hpc/project/crc")
+setwd("/mnt/bigdata/Genetic/Projects/shg047/temp/housekeeping")
+data<-read.csv("https://raw.githubusercontent.com/Shicheng-Guo/esophageal/master/extdata/ESCC-UKBB-RSKAT2020.csv",as.is=T)
+data<-read.csv("https://raw.githubusercontent.com/Shicheng-Guo/colon/master/extdata/UKBB50K/CRC-UKBB-RSKAT2020.csv",as.is=T)
+data<-read.table("https://raw.githubusercontent.com/Shicheng-Guo/ferroptosis/master/NCOA4.tfbs.block.txt",as.is=T,head=F,sep="\t")
+symbol<-data[,1]
+source("https://raw.githubusercontent.com/Shicheng-Guo/GscRbasement/master/Pancancermetadge.R")
+source("https://raw.githubusercontent.com/Shicheng-Guo/GscRbasement/master/pancancermetaOsHr.R")
+source("https://raw.githubusercontent.com/Shicheng-Guo/GscRbasement/master/OROSmerge.R")
+memo="NCOA4-block"
+pancancermetadge(symbol,memo)
+pancancermetaOSHR(symbol,memo)
+OROSmerge(memo)
